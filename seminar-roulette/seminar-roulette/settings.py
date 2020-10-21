9965b8125017d41e7746c47218c2e72efe9e900b
olliegardner <olliegardner@gmail.com>
2020-10-21 20:27:09 +0100

    migrated to postgres database

--- seminar-roulette/seminar-roulette/settings.py
+++ seminar-roulette/seminar-roulette/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'seminar-roulette.wsgi.application'
 DATABASES = {
     'default':
         {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'seminarroulette',
+            'USER': 'seminarroulette_user',
+            'PASSWORD': env('DB_PASSWORD'),
+            'HOST': 'localhost',
+            'PORT': '',
         }
 }
 
