b8faaa70ec449449b345c091fecc5556fdcb0e34
olliegardner <olliegardner@gmail.com>
2020-10-22 18:05:12 +0100

    implemented user context class

--- seminar-roulette/frontend/src/App.js
+++ seminar-roulette/frontend/src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import UserContext from "./context/UserContext";
 import Router from "./Router";
 
 const muiTheme = createMuiTheme({});
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    axios;
+    axios
+      .get(`api/current-user.json`)
+      .then((res) => {
+        setUser(res.data);
+        setLoaded(true);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <MuiThemeProvider theme={muiTheme}>
-      <Router />
+      <UserContext.Provider value={user}>
+        {loaded && <Router />}
+      </UserContext.Provider>
     </MuiThemeProvider>
   );
 };
--- seminar-roulette/frontend/src/views/Landing.js
+++ seminar-roulette/frontend/src/views/Landing.js
@@ -1,28 +1,15 @@
-import React, { Fragment, useEffect, useState } from "react";
-import axios from "axios";
+import React, { Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
+import UserContext from "../context/UserContext";
 
 const Landing = () => {
-  const [user, setUser] = useState({});
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    axios;
-    axios
-      .get(`api/current-user.json`)
-      .then((res) => {
-        setUser(res.data);
-        setLoaded(true);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+  const user = useContext(UserContext);
 
   return (
     <Fragment>
       <h1>Seminar Roulette</h1>
-
-      {loaded && <p>Current user: {user.guid}</p>}
+      <p>Current user: {user.guid}</p>
 
       <Button component={Link} to="/lucky">
         Random Seminar
