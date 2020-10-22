58ce377aa003b8e6d7eac4c27a37b6caf6fdb58f
olliegardner <olliegardner@gmail.com>
2020-10-22 15:31:05 +0100

    created script to get feed from samoa events

--- seminar-roulette/backend/admin.py
+++ seminar-roulette/backend/admin.py
@@ -8,4 +8,28 @@ class UniversityUserAdmin(admin.ModelAdmin):
     list_display = ['guid', 'name', 'is_superuser']
 
 
+class SeminarGroupAdmin(admin.ModelAdmin):
+    list_display = ['name', 'short_name', 'location']
+    readonly_fields = ('url', )
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'latitude', 'longitude', 'online']
+    readonly_fields = ('directions', )
+
+
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ['speaker', 'affiliation']
+    readonly_fields = ('url', )
+
+
+class SeminarAdmin(admin.ModelAdmin):
+    list_display = ['title', 'start_time', 'end_time', 'speaker']
+    readonly_fields = ('description', 'registration_url')
+
+
 admin.site.register(UniversityUser, UniversityUserAdmin)
+admin.site.register(SeminarGroup, SeminarGroupAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Speaker, SpeakerAdmin)
+admin.site.register(Seminar, SeminarAdmin)
--- seminar-roulette/backend/models.py
+++ seminar-roulette/backend/models.py
@@ -17,3 +17,68 @@ class UniversityUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.guid
+
+
+# location of the seminar
+class Location(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    directions = models.TextField(null=True, blank=True)
+    latitude = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True
+    )
+    longitude = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True
+    )
+    online = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ['name', 'latitude', 'longitude']
+
+    def __str__(self):
+        return self.name
+
+
+# seminar hosting group
+class SeminarGroup(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    short_name = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    url = models.URLField(max_length=1000, null=True, blank=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['name', 'location']
+
+    def __str__(self):
+        return self.name
+
+
+# speaker of a seminar
+class Speaker(models.Model):
+    speaker = models.CharField(max_length=255, null=False, blank=False)
+    affiliation = models.CharField(max_length=255, null=True, blank=True)
+    url = models.URLField(max_length=1000, null=True, blank=True)
+
+    class Meta:
+        unique_together = ['speaker', 'affiliation']
+
+    def __str__(self):
+        return self.speaker + ' from ' + self.affiliation
+
+
+# seminar event
+class Seminar(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    registration_url = models.URLField(max_length=1000, null=True, blank=True)
+    start_time = models.DateTimeField(null=False, blank=False)
+    end_time = models.DateTimeField(null=False, blank=False)
+    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
+    seminar_group = models.ForeignKey(SeminarGroup, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['title', 'start_time', 'end_time', 'speaker']
+
+    def __str__(self):
+        return self.title + ' ' + str(self.start_time
+                                     ) + '-' + str(self.end_time)
