2d3219c03721bdea40e1d93aed5fdf4ec0f71e43
olliegardner <olliegardner@gmail.com>
2020-10-26 17:51:47 +0000

    minor improvements to samoa feed script

--- seminar-roulette/event_feed.py
+++ seminar-roulette/event_feed.py
@@ -7,19 +7,35 @@ django.setup()
 from backend.models import *
 import requests
 import json
+import sys
 
 
 class EventFeeds():
     def __init__(self):
-        self.samoa_feed()
+        command_help = 'python event_feed.py [delete, samoa]'
 
-    def samoa_feed(self):
-        print('Retrieving event feed from Samoa. Please wait...')
+        if len(sys.argv) == 2:
+            if sys.argv[1] == 'delete':
+                self.delete_data()
+            elif sys.argv[1] == 'samoa':
+                self.samoa_feed()
+            else:
+                print(command_help)
+        else:
+            print(command_help)
+
+    def delete_data(self):
+        print('Deleting all data. Please wait...')
 
         SeminarGroup.objects.all().delete()
         Location.objects.all().delete()
         Speaker.objects.all().delete()
         Seminar.objects.all().delete()
+
+        print('Data deleted!')
+
+    def samoa_feed(self):
+        print('Retrieving event feed from Samoa. Please wait...')
 
         response = requests.get(
             'https://samoa.dcs.gla.ac.uk/events/rest/Event/searchtext?search='
@@ -62,15 +78,18 @@ class EventFeeds():
             )
 
             seminar, seminar_created = Seminar.objects.get_or_create(
-                title=event['title'],
-                description=event['description'],
-                registration_url=event['registrationUrl'],
-                start_time=event['startTime'],
-                end_time=event['endTime'],
-                speaker=speaker,
-                seminar_group=seminar_group,
-                location=location,
+                samoa_id=event['id']
             )
+            # in case any of these fields change
+            seminar.title = event['title']
+            seminar.description = event['description']
+            seminar.registration_url = event['registrationUrl']
+            seminar.start_time = event['startTime']
+            seminar.end_time = event['endTime']
+            seminar.speaker = speaker
+            seminar.seminar_group = seminar_group
+            seminar.location = location
+            seminar.save()
 
         print('Samoa event feed retrieved!')
 
