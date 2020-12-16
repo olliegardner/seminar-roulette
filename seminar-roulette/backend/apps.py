fb3c0b35590bff284197163b776f4e389304f97b
olliegardner <olliegardner@gmail.com>
2020-12-16 18:11:59 +0000

    re-installed django watson

--- seminar-roulette/backend/apps.py
+++ seminar-roulette/backend/apps.py
@@ -1,8 +1,19 @@
 from django.apps import AppConfig
+from watson import search as watson
 
 
 class BackendConfig(AppConfig):
     name = 'backend'
 
     def ready(self):
-        pass
+        seminar_model = self.get_model('Seminar')
+        watson.register(seminar_model)
+
+        seminar_group_model = self.get_model('SeminarGroup')
+        watson.register(seminar_group_model)
+
+        location_model = self.get_model('Location')
+        watson.register(location_model)
+
+        speaker_model = self.get_model('Speaker')
+        watson.register(speaker_model)
