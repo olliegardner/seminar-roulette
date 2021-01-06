2f5ca4ade557eda0eab77af135c70888bf6613ee
olliegardner <olliegardner@gmail.com>
2021-01-06 15:50:01 +0000

    seminar cards are now expandable on click

--- seminar-roulette/frontend/src/components/SeminarCard.js
+++ seminar-roulette/frontend/src/components/SeminarCard.js
@@ -4,35 +4,51 @@ import moment from "moment";
 import axios from "axios";
 import Cookies from "js-cookie";
 import parse from "html-react-parser";
-import { Link as RouterLink } from "react-router-dom";
 import {
+  Accordion,
+  AccordionActions,
+  AccordionSummary,
+  AccordionDetails,
   Avatar,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardHeader,
   Chip,
-  IconButton,
   makeStyles,
   Tooltip,
   Typography,
 } from "@material-ui/core";
 import Rating from "@material-ui/lab/Rating";
+import { grey } from "@material-ui/core/colors";
 import ClearIcon from "@material-ui/icons/Clear";
 import FastfoodOutlinedIcon from "@material-ui/icons/FastfoodOutlined";
 import LanguageOutlinedIcon from "@material-ui/icons/LanguageOutlined";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import UserContext from "../context/UserContext";
 
 const useStyles = makeStyles((theme) => ({
+  accordion: {
+    "&:hover": {
+      backgroundColor: grey[200],
+    },
+  },
+  accordionColumn: {
+    flexDirection: "column",
+    width: "100%",
+  },
+  accordionDetails: {
+    marginTop: theme.spacing(-2),
+  },
+  avatarContainer: {
+    display: "flex",
+    flex: "0 0 auto",
+    marginRight: theme.spacing(2),
+  },
   avatar: {
     backgroundColor: theme.palette.primary.main,
     width: theme.spacing(7),
     height: theme.spacing(7),
   },
-  fullHeight: {
-    height: "100%",
+  content: {
+    flex: "1 1 auto",
   },
   date: {
     textAlign: "center",
@@ -41,25 +57,97 @@ const useStyles = makeStyles((theme) => ({
   flexGrow: {
     flexGrow: 1,
   },
-  rating: {
+  spaceRight: {
     marginRight: theme.spacing(1),
   },
-  online: {
-    marginRight: theme.spacing(1),
+  header: {
+    display: "flex",
+    alignItems: "center",
   },
-  food: {
-    marginRight: theme.spacing(1),
+  truncatedDescription: {
+    marginTop: theme.spacing(1),
   },
-  seminarDescription: {
-    marginTop: theme.spacing(-2),
-  },
-  keywordChip: {
-    marginRight: theme.spacing(1),
+  seminarActions: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginRight: theme.spacing(-6),
+    marginTop: theme.spacing(1),
   },
 }));
 
 const truncate = (str) => {
-  return str.length > 280 ? str.substring(0, 280) + "..." : str;
+  return str.length > 180 ? str.substring(0, 180) + "..." : str;
+};
+
+const SeminarActions = (props) => {
+  const {
+    seminar,
+    loaded,
+    keywords,
+    currentlyDiscarded,
+    currentRating,
+  } = props;
+  const classes = useStyles();
+
+  return (
+    <>
+      {!seminar.is_future && !currentlyDiscarded && (
+        <Rating
+          name={`rating-${seminar.id}`}
+          defaultValue={currentRating}
+          precision={1}
+          onMouseDown={(e) => e.stopPropagation()}
+          onClick={(e) => e.stopPropagation()}
+          onChange={(e) =>
+            setSeminarAttended(seminar.id, false, e.target.value)
+          }
+        />
+      )}
+
+      <div className={classes.flexGrow} />
+
+      {loaded ? (
+        <>
+          {keywords.slice(0, 3).map((keyword) => (
+            <Chip
+              label={keyword.text}
+              variant="outlined"
+              color="primary"
+              className={classes.spaceRight}
+            />
+          ))}
+        </>
+      ) : (
+        <Chip
+          label="Loading"
+          variant="outlined"
+          color="primary"
+          className={classes.spaceRight}
+        />
+      )}
+
+      {seminar.online && (
+        <Tooltip
+          title="Online seminar"
+          placement="top"
+          className={classes.spaceRight}
+        >
+          <LanguageOutlinedIcon color="secondary" />
+        </Tooltip>
+      )}
+
+      {seminar.serves_food && (
+        <Tooltip
+          title="This seminar serves food!"
+          placement="top"
+          className={classes.spaceRight}
+        >
+          <FastfoodOutlinedIcon color="secondary" />
+        </Tooltip>
+      )}
+    </>
+  );
 };
 
 const SeminarCard = (props) => {
@@ -82,6 +170,7 @@ const SeminarCard = (props) => {
 
   const [keywords, setKeywords] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     axios
@@ -109,132 +198,92 @@ const SeminarCard = (props) => {
   };
 
   return (
-    <Card variant="outlined" className={classes.fullHeight}>
-      <CardActionArea
-        component={RouterLink}
-        to={`/seminar/${seminar.id}`}
-        className={classes.fullHeight}
+    <Accordion
+      variant="outlined"
+      onChange={() => setExpanded(!expanded)}
+      className={classes.accordion}
+    >
+      <AccordionSummary
+        id={`seminar=${seminar.id}`}
+        expandIcon={<ExpandMoreIcon color="primary" />}
       >
-        <CardHeader
-          avatar={
-            <Avatar
-              aria-label="seminar"
-              variant="rounded"
-              className={classes.avatar}
-            >
-              <span className={classes.date}>
-                <b style={{ fontSize: 14 }}>{startDay}</b>
-                <br />
-                {startMonth}
-                <br />
-                {startYear}
-              </span>
-            </Avatar>
-          }
-          action={
+        <div className={classes.accordionColumn}>
+          <div className={classes.header}>
+            <div className={classes.avatarContainer}>
+              <Avatar
+                aria-label="seminar"
+                variant="rounded"
+                className={classes.avatar}
+              >
+                <span className={classes.date}>
+                  <b style={{ fontSize: 14 }}>{startDay}</b>
+                  <br />
+                  {startMonth}
+                  <br />
+                  {startYear}
+                </span>
+              </Avatar>
+            </div>
+
+            <div className={classes.content}>
+              <Typography variant="body2" component="span" display="block">
+                <b style={{ fontSize: 16 }}>{seminar.title}</b>
+              </Typography>
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                component="span"
+                display="block"
+              >
+                {startTime.format("H:mm") +
+                  " - " +
+                  moment(seminar.end_time).format("H:mm")}
+              </Typography>
+            </div>
+          </div>
+
+          {/* {action && <div className={classes.action}>{action}</div>} */}
+
+          {!expanded && (
             <>
-              {!seminar.is_future && (
-                <IconButton
-                  aria-label="clear"
-                  color="secondary"
-                  onMouseDown={(e) => e.stopPropagation()}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    setSeminarAttended(seminar.id, true);
-                  }}
-                  disabled={currentlyDiscarded}
-                >
-                  <Tooltip
-                    title={
-                      currentlyDiscarded
-                        ? "Seminar already discarded"
-                        : "Discard seminar"
-                    }
-                    placement="top"
-                  >
-                    <ClearIcon />
-                  </Tooltip>
-                </IconButton>
-              )}
-            </>
-          }
-          title={<b style={{ fontSize: 16 }}>{seminar.title}</b>}
-          subheader={
-            startTime.format("H:mm") +
-            " - " +
-            moment(seminar.end_time).format("H:mm")
-          }
-        />
+              <Typography
+                variant="body2"
+                component="p"
+                className={classes.truncatedDescription}
+              >
+                {parse(truncate(seminar.description))}
+              </Typography>
 
-        <CardContent>
-          <Typography
-            variant="body2"
-            component="p"
-            className={classes.seminarDescription}
-          >
-            {parse(truncate(seminar.description))}
-          </Typography>
-        </CardContent>
-
-        <CardActions disableSpacing>
-          {!seminar.is_future && !currentlyDiscarded && (
-            <Rating
-              name={`rating-${seminar.id}`}
-              defaultValue={currentRating}
-              precision={1}
-              onMouseDown={(e) => e.stopPropagation()}
-              onClick={(e) => e.stopPropagation()}
-              onChange={(e) =>
-                setSeminarAttended(seminar.id, false, e.target.value)
-              }
-            />
-          )}
-
-          <div className={classes.flexGrow} />
-
-          {loaded ? (
-            <>
-              {keywords.slice(0, 3).map((keyword) => (
-                <Chip
-                  label={keyword.text}
-                  variant="outlined"
-                  color="primary"
-                  className={classes.keywordChip}
+              <div className={classes.seminarActions}>
+                <SeminarActions
+                  seminar={seminar}
+                  loaded={loaded}
+                  keywords={keywords}
+                  currentlyDiscarded={currentlyDiscarded}
+                  currentRating={currentRating}
                 />
-              ))}
+              </div>
             </>
-          ) : (
-            <Chip
-              label="Loading"
-              variant="outlined"
-              color="primary"
-              className={classes.keywordChip}
-            />
           )}
+        </div>
+      </AccordionSummary>
 
-          {seminar.online && (
-            <Tooltip
-              title="Online seminar"
-              placement="top"
-              className={classes.online}
-            >
-              <LanguageOutlinedIcon color="secondary" />
-            </Tooltip>
-          )}
+      <AccordionDetails className={classes.accordionDetails}>
+        <Typography variant="body2" component="p">
+          {parse(seminar.description)}
+        </Typography>
+      </AccordionDetails>
 
-          {seminar.serves_food && (
-            <Tooltip
-              title="This seminar serves food!"
-              placement="top"
-              className={classes.food}
-            >
-              <FastfoodOutlinedIcon color="secondary" />
-            </Tooltip>
-          )}
-        </CardActions>
-      </CardActionArea>
-    </Card>
+      <AccordionActions disableSpacing>
+        <SeminarActions
+          seminar={seminar}
+          loaded={loaded}
+          keywords={keywords}
+          currentlyDiscarded={currentlyDiscarded}
+          currentRating={currentRating}
+        />
+      </AccordionActions>
+    </Accordion>
   );
 };
 
