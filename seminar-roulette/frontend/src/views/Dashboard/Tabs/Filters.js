7ed7b5c3caa2b2e931cff12973b04a4c1369865d
olliegardner <olliegardner@gmail.com>
2021-01-16 17:30:47 +0000

    added sort by button

--- seminar-roulette/frontend/src/views/Dashboard/Tabs/Filters.js
+++ seminar-roulette/frontend/src/views/Dashboard/Tabs/Filters.js
@@ -1,10 +1,9 @@
-import React from "react";
-import { Button, makeStyles } from "@material-ui/core";
+import React, { useState } from "react";
+import { Box, Button, makeStyles, MenuItem, Popover } from "@material-ui/core";
 import SortOutlinedIcon from "@material-ui/icons/SortOutlined";
 
 const useStyles = makeStyles((theme) => ({
   sort: {
-    justifyContent: "flex-end",
     marginRight: theme.spacing(1.5),
   },
   flexGrow: {
@@ -15,6 +14,12 @@ const useStyles = makeStyles((theme) => ({
 const Filters = () => {
   const classes = useStyles();
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const isOpen = Boolean(anchorEl);
+  const handleClose = () => setAnchorEl(null);
+  const handleOpen = (e) => setAnchorEl(e.currentTarget);
+
   return (
     <>
       <div className={classes.flexGrow} />
@@ -23,10 +28,33 @@ const Filters = () => {
         variant="contained"
         startIcon={<SortOutlinedIcon />}
         disableElevation
+        onClick={handleOpen}
         className={classes.sort}
       >
         Sort by
       </Button>
+
+      <Popover
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        anchorEl={anchorEl}
+        open={isOpen}
+        onClose={handleClose}
+        elevation={1}
+      >
+        <Box py={1}>
+          <MenuItem color="inherit">Seminar title A-Z</MenuItem>
+          <MenuItem color="inherit">Seminar title Z-A</MenuItem>
+          <MenuItem color="inherit">Date closest to now</MenuItem>
+          <MenuItem color="inherit">Date farthest from now</MenuItem>
+        </Box>
+      </Popover>
     </>
   );
 };
