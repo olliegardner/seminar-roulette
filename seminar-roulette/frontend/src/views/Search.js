a27f582644eb18b20bc02a4a5313d5286a96e397
olliegardner <olliegardner@gmail.com>
2021-01-29 00:11:42 +0000

    changed search counter bubble to text

--- seminar-roulette/frontend/src/views/Search.js
+++ seminar-roulette/frontend/src/views/Search.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import { Box, Chip, Grid, makeStyles, Typography } from "@material-ui/core";
+import { Box, Grid, makeStyles, Typography } from "@material-ui/core";
 import Pagination from "@material-ui/lab/Pagination";
 
 import SeminarCard from "./../components/SeminarCard";
@@ -24,6 +24,8 @@ const Search = () => {
   const [seminars, setSeminars] = useState([]);
   const [similarities, setSimilarities] = useState({});
   const [loaded, setLoaded] = useState(false);
+
+  const [count, setCount] = useState(0);
   const [page, setPage] = useState(1);
   const [maxPage, setMaxPage] = useState(1);
 
@@ -32,6 +34,7 @@ const Search = () => {
       .get(`/api/search/?q=${search}&page=${page}`)
       .then((res) => {
         setSeminars(res.data.results);
+        setCount(res.data.count);
         setMaxPage(Math.ceil(res.data.count / 10));
 
         if (res.data.count == 0) setLoaded(true);
@@ -60,43 +63,51 @@ const Search = () => {
     <>
       {loaded ? (
         <>
-          <Typography variant="h5">
-            Search Results{" "}
-            <Chip label={seminars.length} size="small" color="secondary" />
-          </Typography>
+          <Typography variant="h5">Search Results</Typography>
           <Typography color="textSecondary" gutterBottom>
             You searched for: {search}
           </Typography>
 
           <Box mt={2}>
             {seminars.length > 0 ? (
-              <Grid container spacing={3} alignItems="center" justify="center">
-                {seminars.map((seminar) => (
-                  <Grid item key={seminar.id} xs={12}>
-                    <SeminarCard
-                      seminar={seminar}
-                      currentRating={null}
-                      currentlyDiscarded={false}
-                      // seminarsUpdated={seminarsUpdated}
-                      // setSeminarsUpdated={setSeminarsUpdated}
-                      similarity={
-                        notAuthenticated ? 0 : similarities[seminar.id]
-                      }
-                    />
-                  </Grid>
-                ))}
+              <>
+                <Typography variant="overline">
+                  <b>{count}</b> {count == 1 ? "seminar" : "seminars"} found...
+                </Typography>
 
-                <Pagination
-                  count={maxPage}
-                  color="primary"
-                  shape="rounded"
-                  showFirstButton
-                  showLastButton
-                  className={classes.pagination}
-                  page={page}
-                  onChange={(e, newPage) => setPage(newPage)}
-                />
-              </Grid>
+                <Grid
+                  container
+                  spacing={3}
+                  alignItems="center"
+                  justify="center"
+                >
+                  {seminars.map((seminar) => (
+                    <Grid item key={seminar.id} xs={12}>
+                      <SeminarCard
+                        seminar={seminar}
+                        currentRating={null}
+                        currentlyDiscarded={false}
+                        // seminarsUpdated={seminarsUpdated}
+                        // setSeminarsUpdated={setSeminarsUpdated}
+                        similarity={
+                          notAuthenticated ? 0 : similarities[seminar.id]
+                        }
+                      />
+                    </Grid>
+                  ))}
+
+                  <Pagination
+                    count={maxPage}
+                    color="primary"
+                    shape="rounded"
+                    showFirstButton
+                    showLastButton
+                    className={classes.pagination}
+                    page={page}
+                    onChange={(e, newPage) => setPage(newPage)}
+                  />
+                </Grid>
+              </>
             ) : (
               <Typography>No seminars found.</Typography>
             )}
